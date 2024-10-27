b24bf6774f07210462d25915f2a74875016eae0b
FlexEbat <52438328+FlexEbat@users.noreply.github.com>
2024-10-27 13:13:02 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+#53
+
+import this
+
+name2 = "Ada Lovelace"
+name = "ada lovelace"
+print(name.title()) #-> Ada Lovelace
+print(name2.upper())
+print(name2.lower())
+
+print("")
+name = "Adam"
+surname = "Adamovich"
+fullname = f"{name} {surname}"
+print(f"hello, {fullname.title()} ")
+message = f"hello, {fullname.title()} "
+print(message)
+
+print("")
+favorite_language = '   python    '
+favorite_language = favorite_language.strip()
+print(favorite_language)
+
+print("")
+print("Bolshoi chlen i ogromniy sis'ki")
+print("")
+universe_age = 14_000_000_000
+x, y, z = 3, 2, 2
+print (x,y,z)
+print(universe_age)
+
+print("")
+AGE = 50
+print(AGE)
+
+print("")
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[1])
+print(bicycles[-1])
+print(bicycles[0].title())
+print(f"Hello, my favourite bicycles is {bicycles[2].title()}")
+#bicycles[1] = "Siski"
+#print(bicycles[1])
+
+
